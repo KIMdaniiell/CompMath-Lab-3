5b39dbc820ceb03df2d14f9b8a5d7919490716a5
Kim Daniel <64163182+KIMdaniiell@users.noreply.github.com>
2022-05-07 21:19:10 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import computation.IntegralCalculator;
+import io.IOManager;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //TODO - точки разрыва (поиск + несколько способов их устранения *при возможности)
+        //TODO - вычисление опримального числа разбиений
+        //TODO - условия существования интеграла
+        IOManager console = new IOManager(new Scanner(System.in));
+        IntegralCalculator iCalculator = new IntegralCalculator(console);
+        iCalculator.calculateRectangleMethod();
+    }
+}
